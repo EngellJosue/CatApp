7986431175905f3a7ded41e56195dedffeef050d
Engell777 <josue@all-win.software>
2023-04-22 18:20:02 -0700

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by User-M02 on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
